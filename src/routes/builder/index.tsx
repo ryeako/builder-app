b83d67ff4eca0e33cd64763d7a8e6ca79c9d7251
Ryan <lordslapnut@gmail.com>
2023-06-29 00:01:27 -0400

    Updating army selection to use GW file

--- src/routes/builder/index.tsx
+++ src/routes/builder/index.tsx
@@ -1,15 +1,31 @@
 import { component$ } from "@builder.io/qwik";
-import armies from './../data/40k10e/armies.json';
 import { Card } from "~/components/army-card";
-import { Link } from "@builder.io/qwik-city";
+import { Link, routeLoader$ } from "@builder.io/qwik-city";
+import { readFile } from 'fs/promises';
+import path from 'path'
+import type { Dump } from "~/models/GwAppInterfaces";
+import { Spinner } from "~/components/spinner";
+
+export const useArmies = routeLoader$(async () => {
+    const filePath = `${path.resolve('./')}\\src\\routes\\data\\40k10e\\dump.json`;
+
+    const file = await readFile(filePath, 'utf8');
+    const data = JSON.parse(file) as Dump;
+
+    return data.data.publication;
+});
 
 export default component$(() => {
+    const armies = useArmies();
+
+    if (armies === undefined || armies.value === undefined) return (<><Spinner /></>);
+
     return (
-        <section class="dark:text-black fixed screen-adjust-width">
+        <section class="dark:text-black fixed overflow-y-auto h-screen">
             <div class="flex flex-row flex-wrap gap-5 m-5">
-            {armies.map((a) => 
+            {armies.value.map((a) => 
                 <Link href={`${a.id}`} key={a.id}>
-                    <Card title={a.name} img={ {src: a.url, alt: a.name }} />
+                    <Card title={a.name} img={ {src: a.factionBackgroundImage, alt: a.name }} />
                 </Link>
             )}
             </div>
